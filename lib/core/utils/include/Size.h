c6fddd53b833cd713d35e3b3b94378912764c018
ValeriiKoniushenko <Valerii.Koniushenko@gmail.com>
2023-06-29 19:01:33 +0300

    Added Size2D and Size3D classes

---
+++ lib/core/utils/include/Size.h
@@ -0,0 +1,51 @@
+// MIT License
+//
+// Copyright (c) 2023 Valerii Koniushenko
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <type_traits>
+
+namespace Utils {
+
+    template<class T>
+    concept Sizeable = std::is_arithmetic_v<T>;
+
+    template<Sizeable T>
+    struct Size2D {
+        T width{};
+        T height{};
+    };
+
+    using DSize2D = Size2D<double>;
+    using FSize2D = Size2D<float>;
+    using ISize2D = Size2D<int>;
+
+    template<Sizeable T>
+    struct Size3D {
+        T width{};
+        T height{};
+        T deep{};
+    };
+
+    using DSize3D = Size2D<double>;
+    using FSize3D = Size2D<float>;
+    using ISize3D = Size2D<int>;
+
+} // namespace Utils
