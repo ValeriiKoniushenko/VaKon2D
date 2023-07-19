011e228cf2adf0d6d00c0f8a5609c6e1b7d79b1d
ValeriiKoniushenko <Valerii.Koniushenko@gmail.com>
2023-07-19 23:32:18 +0300

    Added wrapper for freetype library

--- game/source/VaKon2D.cpp
+++ game/source/VaKon2D.cpp
@@ -40,6 +40,7 @@
 #include "WorldVariables.h"
 #include "ft2build.h"
 #include FT_FREETYPE_H
+#include "FreeTypeLibrary.h"
 
 #include <iostream>
 #include <map>
@@ -112,56 +113,43 @@ void VaKon2D::start()
 	textProgram.link();
 	textProgram.use();
 
-	FT_Library ft;
-	if (FT_Init_FreeType(&ft))
-	{
-		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-		return;
-	}
-
 	FT_Face face;
-	if (FT_New_Face(ft, "assets/fonts/Roboto-Medium.ttf", 0, &face))
+	if (GetFreeTypeLibrary().generateFace("assets/fonts/Roboto-Medium.ttf", 0, face))
 	{
 		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
 		return;
 	}
-	else
+
+	FT_Set_Pixel_Sizes(face, 0, 48);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	for (unsigned char c = 0; c < 128; c++)
 	{
-		// set size to load glyphs as
-		FT_Set_Pixel_Sizes(face, 0, 48);
-
-		// disable byte-alignment restriction
-		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-		// load first 128 characters of ASCII set
-		for (unsigned char c = 0; c < 128; c++)
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
 		{
-			// Load character glyph
-			if (FT_Load_Char(face, c, FT_LOAD_RENDER))
-			{
-				std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-				continue;
-			}
-			// generate texture
-			unsigned int texture;
-			glGenTextures(1, &texture);
-			glBindTexture(GL_TEXTURE_2D, texture);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width, face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE,
-				face->glyph->bitmap.buffer);
-			// set texture options
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			// now store character for later use
-			Character character = {texture, glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-				glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top), static_cast<unsigned int>(face->glyph->advance.x)};
-			Characters.insert(std::pair<char, Character>(c, character));
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
 		}
-		glBindTexture(GL_TEXTURE_2D, 0);
+
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width, face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// now store character for later use
+		Character character = {texture, glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top), static_cast<unsigned int>(face->glyph->advance.x)};
+		Characters.insert(std::pair<char, Character>(c, character));
 	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+
 	FT_Done_Face(face);
-	FT_Done_FreeType(ft);
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
@@ -306,5 +294,6 @@ void VaKon2D::initCore()
 	GetWindow().create({800, 600}, "Game name");	// TODO: get size & title from a config file
 	GladWrapper::initGlad();
 	GetWorld().init();
+	GetFreeTypeLibrary().init();
 	// <---------------------------------------
 }
---
+++ lib/core/freetype-wrapper/source/FreeTypeLibrary.cpp
@@ -0,0 +1,47 @@
+
+// MIT License
+//
+// Copyright (c) 2023 Valerii Koniushenko
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "FreeTypeLibrary.h"
+
+void FreeTypeLibrary::init()
+{
+	if (FT_Init_FreeType(&ft))
+	{
+		throw std::runtime_error("Couldn't initialize freetype library");
+	}
+}
+
+void FreeTypeLibrary::destroy()
+{
+	FT_Done_FreeType(ft);
+}
+
+bool FreeTypeLibrary::generateFace(std::filesystem::path path, FT_Long faceIndex, FT_Face& face)
+{
+	return FT_New_Face(ft, path.string().c_str(), faceIndex, &face);
+}
+
+FreeTypeLibrary& GetFreeTypeLibrary()
+{
+	return FreeTypeLibrary::instance();
+}
