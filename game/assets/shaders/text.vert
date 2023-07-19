116fed9927211387e1361058f913e94a47a30f88
ValeriiKoniushenko <Valerii.Koniushenko@gmail.com>
2023-07-19 23:05:11 +0300

    GLSL refactoring

--- game/assets/shaders/text.vert
+++ game/assets/shaders/text.vert
@@ -1,12 +1,13 @@
 #version 330 core
-layout (location = 0) in vec4 vertex;// <vec2 pos, vec2 tex>
+layout (location = 0) in vec2 aVertex;// <vec2 pos, vec2 tex>
+layout (location = 1) in vec2 aUv;// <vec2 pos, vec2 tex>
+
 out vec2 TexCoords;
 
-uniform mat4 projection;
 uniform vec2 uResolution;
 
 void main()
 {
-    gl_Position = vec4(vertex.xy / uResolution, 0.0, 1.0);
-    TexCoords = vertex.zw;
+    gl_Position = vec4(aVertex / uResolution, 0.0, 1.0);
+    TexCoords = aUv;
 }
--- game/source/VaKon2D.cpp
+++ game/source/VaKon2D.cpp
@@ -77,9 +77,8 @@ void RenderText(ShaderProgram& shader, std::string text, float x, float y, float
 		float w = ch.Size.x * scale;
 		float h = ch.Size.y * scale;
 		// update VBO for each character
-		float vertices[6][4] = {{xpos, ypos + h, 0.0f, 0.0f}, {xpos, ypos, 0.0f, 1.0f}, {xpos + w, ypos, 1.0f, 1.0f},
-
-			{xpos, ypos + h, 0.0f, 0.0f}, {xpos + w, ypos, 1.0f, 1.0f}, {xpos + w, ypos + h, 1.0f, 0.0f}};
+		float vertices[24] = {xpos, ypos + h, 0.0f, 0.0f, xpos, ypos, 0.0f, 1.0f, xpos + w, ypos, 1.0f, 1.0f, xpos, ypos + h, 0.0f,
+			0.0f, xpos + w, ypos, 1.0f, 1.0, xpos + w, ypos + h, 1.0f, 0.0f};
 		// render glyph texture over quad
 		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
 		// update content of VBO memory
@@ -170,7 +169,9 @@ void VaKon2D::start()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*) (2 * sizeof(float)));
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
