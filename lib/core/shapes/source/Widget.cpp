54cc7258a0cb6c1bdbf12ee53b6e84dfd44e43ce
ValeriiKoniushenko <Valerii.Koniushenko@gmail.com>
2023-07-25 23:19:29 +0300

    Buf fixing

--- lib/core/shapes/source/Widget.cpp
+++ lib/core/shapes/source/Widget.cpp
@@ -54,7 +54,9 @@ void Widget::draw(ShaderPack& shaderPack)
 	}
 
 	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::translate(trans, glm::vec3(position_, 0.f));
+	trans = glm::translate(trans, glm::vec3(position_ / glm::vec2(static_cast<float>(GetWindow().getSize().width),
+															static_cast<float>(GetWindow().getSize().height)),
+									  0.f));
 	trans = glm::rotate(trans, rotation_, glm::vec3(0.0f, 0.0f, 1.0f));
 
 	shaderProgram.use();
